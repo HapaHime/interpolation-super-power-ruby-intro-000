24de0dcce3ffe3cbdd1d4a874ede90b2e8a17ed3
sduome@yahoo.com <sduome@yahoo.com>
2018-05-10 02:30:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,2 +1,4 @@
-display_rainbow(["red", "orange", "yellow", "green", "indigo", "violet"])
+def display_rainbow(["red", "orange", "yellow", "green", "indigo", "violet"])
   puts "R:#{display_rainbow[0]}, O:#{display_rainbow[1]}, Y:#{display_rainbow[2]}, G:#{display_rainbow[3]}, I:#{display_rainbow[4]}, V:#{display_rainbow[5]}"
+end
+puts display_rainbow
