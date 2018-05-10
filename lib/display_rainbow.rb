34e6955a353f6c9022e2d2194b2a069160dd0177
sduome@yahoo.com <sduome@yahoo.com>
2018-05-10 02:27:04 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,2 @@
 display_rainbow = ["red", "orange", "yellow", "green", "indigo", "violet"]
   puts "R:#{display_rainbow[0]}, O:#{display_rainbow[1]}, Y:#{display_rainbow[2]}, G:#{display_rainbow[3]}, I:#{display_rainbow[4]}, V:#{display_rainbow[5]}"
-
